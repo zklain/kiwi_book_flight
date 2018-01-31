f5ad35c282362381621037c9fefb59b487733e84
zklain <zdnkk@protonmail.com>
2018-01-31 14:08:12 +0100

    skypicker api call working

--- book_flight.py
+++ book_flight.py
@@ -8,21 +8,29 @@ import sys
 def convert_date_format(date):
     new_format = ''
     date_split = date.split('-')
-    new_format += date_split[2] + '/'
-    new_format += date_split[1]
-    new_format += date_split[0]
+    new_format += date_split[0] + '/'
+    new_format += date_split[1] + '/'
+    new_format += date_split[2]
+    return new_format
 
 def book_flight(args):
-    r = requests.get('https://api.skypicker.com/flights?flyFrom=BTS&to=SXF&dateFrom=30/1/2018&dateTo=1/2/2018&partner=picky')
+    date = convert_date_format(args.date)
+    get_r_string = 'https://api.skypicker.com/flights?flyFrom={}&to={}&dateFrom={}&dateTo=1/2/2018&sort=price'.format(args.from_f, args.to, date)
+    print(get_r_string)
+    r = requests.get(get_r_string)
     print(r.status_code)
-    print(json.dumps(r.json(), indent=4))
+    cheapest = r.json()['data'][0]
+
+    print('Cheapest form BTS to A')
+    print(json.dumps(cheapest, indent=4))
+    # print(json.dumps(r.json(), indent=4))
 
 
 def main():
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("--date", type=str, required=True, help='date in YYYY-MM-DD format')
-    arg_parser.add_argument("--from", type=str, required=True, help='airfield IATA code')
-    arg_parser.add_argument("--to", type=str, required=True, help='destination airfield IATA code')
+    arg_parser.add_argument("--date", '-d', type=str, required=True, help='date in YYYY-MM-DD format')
+    arg_parser.add_argument('--from' '-f', type=str, required=True, help='airfield IATA code')
+    arg_parser.add_argument("--to", '-t', type=str, required=True, help='destination airfield IATA code')
     # optional arguments
     arg_parser.add_argument("--one-way", action="store_true", default='True', required=False, \
                 help='no return ticket (default option)')
@@ -33,6 +41,8 @@ def main():
 
 
     args = arg_parser.parse_args()
+    print(args.from_f)
+    print(args.to)
 
     book_flight(args)
 
